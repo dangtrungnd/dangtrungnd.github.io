f5c98aac2742a3822d6018241539c918e8c65312
dangtrungnd <32924847+dangtrungnd@users.noreply.github.com>
2019-01-21 16:47:08 +0700

    push new code

---
+++ .gitignore/main.js
@@ -0,0 +1,82 @@
+//client
+const socket = io('https://video3306.herokuapp.com/');
+
+$('#infoCall').hide();
+socket.on('list_user_online', arrUserInfo =>{
+	$('#infoCall').show();
+	$('#signUp').hide();
+	arrUserInfo.forEach(user => {
+		const {name, peerId} = user;
+		$('#onUser').append(`<li id ="${peerId}">${name}</li>`);
+	});
+});
+
+socket.on('has_new_user_online', user =>{
+	const {name, peerId} = user;
+	$('#onUser').append(`<li id ="${peerId}">${name}</li>`);
+});
+
+socket.on('has_user_out', peerId =>{
+	$(`#${peerId}`).remove();
+})
+
+socket.on('register_faild', () =>  alert('Name is exist, please choose new name!'));
+
+function openStream() {
+	const config = { audio:false, video:true };
+	return navigator.mediaDevices.getUserMedia(config);
+}
+function playStream(idVideoTag, stream) {
+	const video = document.getElementById(idVideoTag);
+	video.srcObject = stream;
+	video.play();
+}
+
+
+
+//peer
+
+const	peer = new Peer({
+			key: 'peerjs',
+			host: "my-peer3306.herokuapp.com",
+			port: 443,
+			secure: true
+		});
+//get peer id
+peer.on('open', id => {
+    $('#my-peer').append(id);
+    $('#btnSignUp').click(() => {
+	    const userName = $('#txtUserName').val();
+	    socket.emit('user_register', { name: userName, peerId: id });
+	});
+});
+
+//call
+$('#btnCall').click(() =>{
+	const id = $('#remoteId').val();
+	openStream()
+	.then( stream =>{
+		playStream('localStream', stream);
+		const call = peer.call(id, stream);
+		call.on('stream', remoteStream => playStream('remoteStream', remoteStream));
+	});
+});
+
+peer.on('call' , call =>{
+	openStream()
+	.then(stream => {
+		call.answer(stream);
+		playStream('localStream', stream);
+		call.on('stream', remoteStream  => playStream('remoteStream', remoteStream));
+	});
+});
+
+$('#onUser').on('click', 'li', function(){
+	const id = $(this).attr('id');
+	openStream()
+	.then( stream =>{
+		playStream('localStream', stream);
+		const call = peer.call(id, stream);
+		call.on('stream', remoteStream => playStream('remoteStream', remoteStream));
+	});
+});
